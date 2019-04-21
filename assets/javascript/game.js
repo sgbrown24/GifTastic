b69b7b28f13fe9527bc98d0ced4b5621c32fc2f9
Serena Brown <serenagbrown24@gmail.com>
2019-04-21 13:43:15 -0600

    add a player button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,48 @@
 $(document).ready(function () {
 $("#add-player").on("click", function(event) {
     event.preventDefault();
-    console.log("looking for click");
+    // console.log("looking for click");
 var addPlayer = $("#player-input").val()
-console.log(addPlayer);
+console.log('ADDED PLAYER', addPlayer);
 
-$("#buttonDiv").append(`<button class = "newButton" data-player=${addPlayer}>${addPlayer}</button>
+$("#buttonDiv").append(`<button class = "newButton" data-player="${addPlayer}">${addPlayer}</button>
 `)
 })
-  $("button").on("click", function() {
-    //   Event.preventDefault();
-      var player = $(this).attr("data-player");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      encodeURIComponent(player.trim()) + "&api_key=ho2NR3gmJEeQLRwM5NiKqJDqjyUF0Nas&limit=10";
-    //   AJAX request with the queryURL
-      console.log(queryURL)
-      console.log(player);
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-      })
+
+$(document).on('click', 'button', function() {
+  console.log('fired!')
+  //   Event.preventDefault();
+    var player = $(this).attr("data-player");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    encodeURIComponent(player.trim()) + "&api_key=ho2NR3gmJEeQLRwM5NiKqJDqjyUF0Nas&limit=10";
+  //   AJAX request with the queryURL
+    console.log(queryURL)
+    console.log(player);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
 .then(function(response) {
-    var results = response.data;
+  var results = response.data;
 console.log(results);
-    for (var i = 0; i < results.length; i++ ){
-        var playerDiv = $("<div>");
-        //making a paragraph tag with the result items
-        // var p = $("p").text("Rating: " + results[i].rating); 
-        //creating image tag
-        var playerImage = $("<img>");
-        playerImage.attr("src",results[i].images.fixed_height.url);
-        playerDiv.prepend(playerImage);
-         
-        $("#gifArea").prepend(playerDiv);
-        
-    }
+  for (var i = 0; i < results.length; i++ ){
+      var playerDiv = $("<div>");
+      //making a paragraph tag with the result items
+      // var p = $("p").text("Rating: " + results[i].rating); 
+      //creating image tag
+      var playerImage = $("<img>");
+      playerImage.attr("src",results[i].images.fixed_height.url);
+      playerDiv.prepend(playerImage);
+       
+      $("#gifArea").prepend(playerDiv);
+      
+  }
 
 
 })
-  })
+});
+
+  // $("button").on("click", function() {
+    
+  // })
 })
